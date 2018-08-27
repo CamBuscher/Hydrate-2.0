069b14a482106bde056614fe7c9d21fb439f127d
Cameron Buscher <cameronbuscher@gmail.com>
2018-08-27 13:32:59 -0700

    Add a LogIn component, and test file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
 
+import LogIn from './LogIn/LogIn';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, Butthole </h1>
+        <LogIn />
       </div>
     );
   }
---
+++ src/LogIn/LogIn.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './LogIn.css';
+
+class LogIn extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <p>hi court</p>
+      </div>
+    )
+  }
+}
+
+export default LogIn;
