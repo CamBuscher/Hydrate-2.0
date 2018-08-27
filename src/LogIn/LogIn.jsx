5c85107c300d84a915beeb803fc1ccb95a4257e0
Cameron Buscher <cameronbuscher@gmail.com>
2018-08-27 13:56:30 -0700

    Add a button for logging in, with styles borrowed from a very cool sprinkler company's website

--- src/LogIn/LogIn.jsx
+++ src/LogIn/LogIn.jsx
@@ -23,6 +23,7 @@ class LogIn extends Component {
       <div className='LogIn'>
         <h2>Please enter your account's API key to see your devices</h2>
         <input type="text" value={this.state.key} name='key' onChange={this.handleChange}/>
+        <button>Log In</button>
         <p><a href='' target='_blank'>Can't find your API key?</a></p>
       </div>
     )
