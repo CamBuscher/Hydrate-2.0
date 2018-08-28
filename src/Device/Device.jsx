a7b56cc29547a12e4db684c1e7f5d41ba0c52eea
Cameron Buscher <cameronbuscher@gmail.com>
2018-08-28 17:41:17 -0600

    Add some rendering of zone information, including random images of waterfowl with arms

--- src/Device/Device.jsx
+++ src/Device/Device.jsx
@@ -13,9 +13,21 @@ export default class Device extends Component {
   render() {
     const {device} = this.props;
 
+    const renderZones = () => {
+      return device.zones.map(zone => {
+        return (
+          <div className='device_zone'>
+            <img src={zone.imageUrl} alt={`Image of ${zone.name}`} className='zone_image'/>
+            <h4 className='zone_name'>{zone.name}</h4>
+          </div>
+        )
+      })
+    }
+
     return(
       <div className='device'>
         <div className='device_name'>{device.name}</div>
+        {renderZones()}
       </div>
     )
   }
