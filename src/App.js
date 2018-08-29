4fa11a9b5fda3e6cb0eb45a91d8f13c69b48bb66
Cameron Buscher <cameronbuscher@gmail.com>
2018-08-28 23:14:58 -0600

    Add janky error handling for if API key is entered incorrectly

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
       loading: false,
       userID: null,
       user: null,
-      apiKey: null
+      apiKey: null,
+      loadingError: null
     }
 
     this.getID = this.getID.bind(this);
@@ -20,32 +21,43 @@ class App extends Component {
 
   getID(apiKey) {
     this.setState({ loading: true, apiKey })
-    fetch('https://api.rach.io/1/public/person/info', {
-      headers: {Authorization: `Bearer ${apiKey}`}
-    })
-      .then(response => response.json())
-      .then(userID => {
-        this.setState({ userID })
-        fetch(`https://api.rach.io/1/public/person/${userID.id}`, {
-          headers: { Authorization: `Bearer ${apiKey}` }
-        })
-          .then(response => response.json())
-          .then(user => {
-            this.setState({ loading: false, user})
-          })
+    try {
+      fetch('https://api.rach.io/1/public/person/info', {
+        headers: {Authorization: `Bearer ${apiKey}`}
       })
+        .then(response => response.json())
+        .then(userID => {
+          this.setState({ userID })
+          fetch(`https://api.rach.io/1/public/person/${userID.id}`, {
+            headers: { Authorization: `Bearer ${apiKey}` }
+          })
+            .then(response => response.json())
+            .then(user => {
+              if (user.errors) {
+                this.setState({
+                  loadingError: 'User not found, please check your key',
+                  loading: false
+                })
+              } else {
+                this.setState({ loading: false, user})
+              }
+            })
+        })
+       } catch(error) {
+         console.log(error)
+    }
   }
 
   render() {
     const rendered = () => {
-      const {loading, user} = this.state;
+      const {loading, user, loadingError} = this.state;
 
       if (loading) {
         return <img src='/Eclipse.gif' className='loading_gif'/>
       } else if (user) {
         return <Devices userID={user.id} devices={user.devices} apiKey={this.state.apiKey} />
       } else {
-        return <LogIn getID={this.getID} />
+        return <LogIn getID={this.getID} loadingError={loadingError}/>
       }
     }
 
